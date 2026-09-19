5dfec1561b7b8361c6b7d4bf6309a3b2d877765a
1rochajonas2-ctrl <1rochajonas2@gmail.com>
2026-09-18 22:07:07 -0300

    Create headers.js

---
+++ src/config/headers.js
@@ -0,0 +1,17 @@
+module.exports = {
+  headers: {
+    Platform: 'pc',
+    Language: 'en',
+    Accept: 'application/json',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 Chrome/120.0.0.0 Mobile Safari/537.36'
+  },
+  WFM_HEADERS: {
+    Platform: 'pc',
+    Language: 'en',
+    Accept: 'application/json',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    Origin: 'https://warframe.market',
+    Referer: 'https://warframe.market/',
+    'Accept-Language': 'en-US,en;q=0.9'
+  }
+}
